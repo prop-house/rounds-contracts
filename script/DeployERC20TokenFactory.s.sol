4e6b8496176808b54906600ac27e0328598f9008
solimander <solimander-dev@protonmail.com>
2024-09-18 02:10:08 +0100

    Deployment script

---
+++ script/DeployERC20TokenFactory.s.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import {Script} from 'forge-std/Script.sol';
+import {ERC20Token} from 'src/tokens/ERC20Token.sol';
+import {ERC20TokenFactory} from 'src/tokens/ERC20TokenFactory.sol';
+import {UpgradeableBeacon} from 'openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol';
+import {ERC1967Proxy} from 'openzeppelin-contracts/contracts/proxy/ERC1967/ERC1967Proxy.sol';
+
+contract DeployUserPayoutEscrowFactory is Script {
+    function run() public returns (ERC20TokenFactory factory) {
+        uint256 deployerKey = vm.envUint('DEPLOYER_PRIVATE_KEY');
+        vm.startBroadcast(deployerKey);
+
+        address owner = vm.envAddress('OWNER_ADDRESS');
+
+        bytes32 salt = bytes32(uint256(42));
+
+        // forgefmt: disable-next-item
+        address erc20TokenBeacon = address(new UpgradeableBeacon{salt: salt}(address(new ERC20Token{salt: salt}()), owner));
+        address factoryImpl = address(new ERC20TokenFactory{salt: salt}(erc20TokenBeacon));
+
+        factory = ERC20TokenFactory(
+            address(new ERC1967Proxy{salt: salt}(factoryImpl, abi.encodeCall(ERC20TokenFactory.initialize, (owner))))
+        );
+        vm.stopBroadcast();
+    }
+}
